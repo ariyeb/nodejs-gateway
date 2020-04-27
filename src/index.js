a7b5d80bdc7fa01df340d9b281b575a076552218
Ariye Bar-tov <ariyeb@gmail.com>
2020-04-27 23:08:23 +0300

    commit: 02

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const weatherRouter = require('./routers/weatherRouter')
+const authRouter = require('./routers/authRouter')
 const port = process.env.PORT
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(weatherRouter)
+app.use(authRouter)
 
 app.listen(port, () => {
     console.log('Server connected, port: ' + port)
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,3 +1,64 @@
 const express = require('express')
+const tiny = require('tiny-json-http')
+
+const URL = "http://ec2-34-244-57-90.eu-west-1.compute.amazonaws.com:3000"
 
 const router = express.Router()
+
+router.post('/auth-service/signup', async (req, res) => {
+    const data = req.body
+    const url = URL + "/signup"
+
+    try {
+        const response = (await tiny.post({ url, data })).body
+        res.status(201).send(response)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/auth-service/login', async (req, res) => {
+    const data = req.body
+    const url = URL + "/login"
+
+    try {
+        const response = (await tiny.post({ url, data })).body
+        res.send(response)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/auth-service/new-token', async (req, res) => {
+    const url = URL + "/new-token"
+    const headers = req.headers
+
+    try {
+        const response = (await tiny.post({ url, headers })).body
+        res.send(response)
+    } catch (e) {
+        if (e.statusCode === 401) {
+            res.status(401).send()
+        } else {
+            res.status(400).send()
+        }
+    }
+})
+
+router.get('/auth-service/check-token', async (req, res) => {
+    const url = URL + "/check-token"
+    const headers = req.headers
+
+    try {
+        const response = (await tiny.get({ url, headers })).body
+        res.send(response)
+    } catch (e) {
+        if (e.statusCode === 401) {
+            res.status(401).send()
+        } else {
+            res.status(400).send()
+        }
+    }
+})
+
+module.exports = router
